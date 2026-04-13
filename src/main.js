14cff09f3fa94411d242cbae1fc4803d5437de68
evyhdev <evelypaz2010@gmail.com>
2026-04-13 16:12:20 -0300

    feat: estrutura inicial da header responsiva

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// IMPORTAÇÃO DE ESTILOS
+// Isso faz o Vite injetar o CSS no seu HTML automaticamente
+import './css/global.css'
+// NOTA PARA A EQUIPE:
+// Nesta Fase 1, não adicionaremos lógica de funções aqui.
+// Apenas garantiremos que o servidor do Vite esteja rodando corretamente.
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        // Caminhos atualizados para incluir /src/
+        adotar: resolve(__dirname, "src/pages/adotar.html"),
+        ajudar: resolve(__dirname, "src/pages/ajudar.html"),
+        noticias: resolve(__dirname, "src/pages/noticias.html"),
+        sobre: resolve(__dirname, "src/pages/sobre.html"),
+        voluntario: resolve(__dirname, "src/pages/voluntario.html"),
+      },
+    },
+  },
+});
