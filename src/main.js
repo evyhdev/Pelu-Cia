e06b1ccbe9793c5958f108775b7cb1346ff8ca0f
evyhdev <evelypaz2010@gmail.com>
2026-05-10 22:17:57 -0300

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,1 @@
-// IMPORTAÇÃO DE ESTILOS
-// Isso faz o Vite injetar o CSS no seu HTML automaticamente
 import './css/global.css'
-// NOTA PARA A EQUIPE:
-// Nesta Fase 1, não adicionaremos lógica de funções aqui.
-// Apenas garantiremos que o servidor do Vite esteja rodando corretamente.
